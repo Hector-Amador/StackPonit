9b37608e24e507a99498b6188354653c7df38ad9
Hector Amador <hecttor9@gmail.com>
2026-07-30 16:37:42 -0600

    Fix incorrect hostname in Next.js image domains configuration

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
             },
             {
                 protocol: "https",
-                hostname: "https://stackpoint.netlify.app/",
+                hostname: "stackpoint.netlify.app/",
             }
         ],
     },
